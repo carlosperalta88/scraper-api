59218d629054d97fbe12f9f6f46cb78e44fc99ce
Carlos <carlosperalta88@gmail.com>
2020-05-07 23:37:39 -0400

    return case role when saved

--- src/controllers/CasesDataController.js
+++ src/controllers/CasesDataController.js
@@ -10,7 +10,7 @@ exports.add = async (req, res) => {
     const [updatedCase] = await CasesDataService.add(req)
     logger.info(`${req.params.role} saved`)
     ObservableInsert.checkInsert(updatedCase)
-    res.json(updatedCase).status(204)
+    res.json({ case: req.params.role }).status(204)
     return 
   } catch (error) {
     logger.info(`failed saving the cause ${error}`)
