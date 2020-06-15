efd7656ddafe62f24a9bb48641da0e0b9c460c32
Carlos <carlosperalta88@gmail.com>
2020-06-15 17:51:21 -0400

    upper case E

--- src/routes/executions.js
+++ src/routes/executions.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var authController = require('../controllers/AuthController')
-var executionsController = require('../controllers/executionsController')
+var executionsController = require('../controllers/ExecutionsController')
 
 router.post('/', authController.APIKey, executionsController.create)
 router.get('/:id', authController.APIKey, executionsController.get)
